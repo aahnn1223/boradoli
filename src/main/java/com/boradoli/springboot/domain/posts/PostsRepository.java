d3ec8d8ed3e0607dcc5748c8dc3eb1d1a5275b0a
tmdxo1274 <aahnn1223@gmail.com>
2023-09-04 14:43:52 +0900

    Posts Entity 추가

---
+++ src/main/java/com/boradoli/springboot/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.boradoli.springboot.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {
+}
