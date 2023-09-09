ac794e64ba1589d1d815c575ab5b8f61cb36b971
tmdxo1274 <aahnn1223@gmail.com>
2023-09-09 22:38:34 +0900

    주석 추가

--- src/main/java/com/boradoli/springboot/domain/posts/BaseTimeEntity.java
+++ src/main/java/com/boradoli/springboot/domain/posts/BaseTimeEntity.java
@@ -10,12 +10,13 @@ import javax.persistence.MappedSuperclass;
 import java.time.LocalDateTime;
 
 @Getter
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass                                   // JPA Entity 클래스들이 BaseTimeEntity을 상속할 경우 필드들(createdDate, modifiedDate)도 칼럼으로 인식하도록 함
+@EntityListeners(AuditingEntityListener.class)      // BaseTimeEntity 클래스에 Auditing 기능 포함시킴
 public abstract class BaseTimeEntity {
-    @CreatedDate
-    private LocalDateTime createDate;
 
-    @LastModifiedDate
+    @CreatedDate                                    // Entity가 생성되어 저장될 때 시간이 자동 저장됨
+    private LocalDateTime createdDate;
+
+    @LastModifiedDate                               // 조회한 Entity의 값이 변경될 때 시간이 자동 저장됨
     private LocalDateTime modifiedDate;
 }
