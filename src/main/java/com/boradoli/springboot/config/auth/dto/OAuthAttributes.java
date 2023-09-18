e61643e08d08d3847fb976b135aeef7b354f2cc5
Seungtae <aahnn1223@naver.com>
2023-09-18 14:29:13 +0900

    OAuth2 네이버 로그인 추가

--- src/main/java/com/boradoli/springboot/config/auth/dto/OAuthAttributes.java
+++ src/main/java/com/boradoli/springboot/config/auth/dto/OAuthAttributes.java
@@ -28,6 +28,9 @@ public class OAuthAttributes {
     }
 
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id", attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
     }
     public static OAuthAttributes ofGoogle(String userNameAttribute, Map<String, Object> attributes){
@@ -37,6 +40,18 @@ public class OAuthAttributes {
                 .picture((String)attributes.get("picture"))
                 .attributes(attributes)
                 .nameAttributeKey(userNameAttribute)
+                .build();
+    }
+
+    public static OAuthAttributes ofNaver(String usernameAttributeName, Map<String, Object> attributes){
+        Map<String, Object> response = (Map<String, Object>)attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String) response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(usernameAttributeName)
                 .build();
     }
 
