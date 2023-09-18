073ea4ef7f19924821834c6f52764f4c3c592053
Seungtae <aahnn1223@naver.com>
2023-09-18 13:33:48 +0900

    중복 작성되어 있는 getSession 코드를 어노테이션을 활용하여 개선

--- src/main/java/com/boradoli/springboot/web/IndexController.java
+++ src/main/java/com/boradoli/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.boradoli.springboot.web;
 
+import com.boradoli.springboot.config.auth.LoginUser;
 import com.boradoli.springboot.config.auth.dto.SessionUser;
 import com.boradoli.springboot.service.posts.PostsService;
 import com.boradoli.springboot.web.dto.PostsResponseDto;
@@ -18,9 +19,9 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginUser SessionUser user){
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
+//        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null){
             model.addAttribute("userName", user.getName());
         }
