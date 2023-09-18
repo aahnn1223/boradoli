7889a0dd0fa82e684beadf8e69206b8e79cb1ce3
Seungtae <aahnn1223@naver.com>
2023-09-18 15:32:15 +0900

    - Test 용 properties 생성 - Mock 사용하여 임의 사용자 인증 추가 - WebMvcTest 사용하는 HelloControllerTest 에서 CustomOAuthUserService가 스캔 대상이 아니라 SecurityConfig 를 생성하기 위해 필요한 CustomOAuthUserService를 읽지아 SecurityConfig를 스캔대상에서 제거 - Application.java의 @EnableJpaAuditing을 사용하기 위해 최소 한개의 Entity가 필요하나 WebMvcTest이다 보니 당연히 없다. JpaConfig 생성하여 @EnableJpaAuditing을 분리

--- src/test/java/com/boradoli/springboot/web/HelloControllerTest.java
+++ src/test/java/com/boradoli/springboot/web/HelloControllerTest.java
@@ -1,9 +1,13 @@
 package com.boradoli.springboot.web;
 
+import com.boradoli.springboot.config.auth.SecurityConfig;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -12,13 +16,17 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
-@WebMvcTest(controllers = HelloController.class)
+@WebMvcTest(controllers = HelloController.class,
+excludeFilters = {
+        @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = SecurityConfig.class)
+})
 class HelloControllerTest {
 
     @Autowired
     private MockMvc mvc;
 
     @Test
+    @WithMockUser(roles="USER")
     public void hello가_리턴된다() throws Exception{
         String hello = "hello";
 
@@ -29,6 +37,7 @@ class HelloControllerTest {
     }
 
     @Test
+    @WithMockUser(roles="USER")
     public void helloDto가_리턴된다() throws Exception {
         String name = "hello";
         int amount = 1000;
